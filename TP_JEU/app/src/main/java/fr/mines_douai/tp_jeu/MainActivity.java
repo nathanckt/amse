84a836007b86f8ab932fb2f9e6a1b9121b7203d0
Nathan Crincket <crincketnathan@gmail.com>
2025-03-03 10:38:51 +0100

    mouvement-vaisseau

--- TP_JEU/app/src/main/java/fr/mines_douai/tp_jeu/MainActivity.java
+++ TP_JEU/app/src/main/java/fr/mines_douai/tp_jeu/MainActivity.java
@@ -1,11 +1,12 @@
 package fr.mines_douai.tp_jeu;
 
 import android.animation.ObjectAnimator;
-import android.graphics.drawable.Drawable;
+import android.graphics.Rect;
 import android.os.Bundle;
 import android.os.Handler;
 import android.util.Log;
-import android.view.ViewGroup;
+import android.view.MotionEvent;
+import android.view.View;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
 import androidx.appcompat.app.AppCompatActivity;
@@ -16,7 +17,6 @@ public class MainActivity extends AppCompatActivity {
     private Handler handler = new Handler();
     private Random random = new Random();
 
-    // Images d'asteroïdes
     private int[] asteroidImages = {
             R.drawable.asteroid1,
             R.drawable.asteroid2,
@@ -24,60 +24,124 @@ public class MainActivity extends AppCompatActivity {
             R.drawable.asteroid4
     };
 
+    private ImageView spaceship;
+    private ImageView joystick;
+    private float joystickInputX = 0;
+    private float joystickInputY = 0;
+    private float spaceshipX;
+    private float spaceshipY;
+    private float speed = 10;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState){
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         gameLayout = findViewById(R.id.gameLayout);
+        spaceship = findViewById(R.id.spaceship);
+        joystick = findViewById(R.id.joystick);
 
+        spaceshipX = spaceship.getX();
+        spaceshipY = spaceship.getY();
+
+        joystick.setOnTouchListener((v, event) -> {
+            float centerX = joystick.getWidth() / 2f;
+            float centerY = joystick.getHeight() / 2f;
+            float touchX = event.getX();
+            float touchY = event.getY();
+
+            joystickInputX = (touchX - centerX) / centerX;
+            joystickInputY = (touchY - centerY) / centerY;
+
+            joystickInputX = Math.max(-1, Math.min(1, joystickInputX));
+            joystickInputY = Math.max(-1, Math.min(1, joystickInputY));
+
+            if (event.getAction() == MotionEvent.ACTION_UP) {
+                joystickInputX = 0;
+                joystickInputY = 0;
+                v.performClick();
+            }
+            return true;
+        });
+
+        startGameLoop();
         startAsteroidSpawner();
     }
 
-    private void startAsteroidSpawner(){
+    private void startGameLoop() {
         handler.postDelayed(new Runnable() {
             @Override
-            public void run(){
+            public void run() {
+                updateSpaceshipPosition();
+                checkCollisions();
+                handler.postDelayed(this, 16);
+            }
+        }, 16);
+    }
+
+    private void updateSpaceshipPosition() {
+        spaceshipX += joystickInputX * speed;
+        spaceshipY += joystickInputY * speed;
+
+        spaceshipX = Math.max(0, Math.min(gameLayout.getWidth() - spaceship.getWidth(), spaceshipX));
+        spaceshipY = Math.max(0, Math.min(gameLayout.getHeight() - spaceship.getHeight(), spaceshipY));
+
+        spaceship.setX(spaceshipX);
+        spaceship.setY(spaceshipY);
+    }
+
+    private void startAsteroidSpawner() {
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
                 spawnAsteroid();
-                handler.postDelayed(this, 1000); // Toutes les secondes
+                handler.postDelayed(this, 1000);
             }
         }, 1000);
     }
 
-    private void spawnAsteroid(){
-        Log.d("DEBUG", "Astéroïde créé !");
-        // Création d'un asteroïde
+    private void spawnAsteroid() {
         ImageView asteroid = new ImageView(this);
         asteroid.setImageResource(asteroidImages[random.nextInt(asteroidImages.length)]);
-        if (asteroid.getDrawable() == null) {
-            Log.e("ERROR", "Image non trouvée !");
-        }
-
-        // Définition de la taille de l'astéroïde
         int size = random.nextInt(100) + 50;
         FrameLayout.LayoutParams params = new FrameLayout.LayoutParams(size, size);
-
-        // Définition de la position de X en aléatoire
         int startX = random.nextInt(gameLayout.getWidth() - size);
         params.leftMargin = startX;
         params.topMargin = -size;
         asteroid.setLayoutParams(params);
-        Log.d("DEBUG", "Position X : " + startX);
-
-        // Ajouter l'astéroïde au layout
         gameLayout.addView(asteroid);
 
-        // Lancer la descente
         ObjectAnimator animator = ObjectAnimator.ofFloat(asteroid, "translationY", gameLayout.getHeight());
-        animator.setDuration(3000); // temps de descente
+        animator.setDuration(3000);
         animator.start();
 
-        // Supprimer l'astéroïde après l'animation
         animator.addListener(new android.animation.AnimatorListenerAdapter() {
             @Override
             public void onAnimationEnd(android.animation.Animator animation) {
                 gameLayout.removeView(asteroid);
             }
         });
+    }
+
+    private void checkCollisions() {
+        for (int i = 0; i < gameLayout.getChildCount(); i++) {
+            View view = gameLayout.getChildAt(i);
+            if (view instanceof ImageView && view != spaceship) {
+                if (checkCollision((ImageView) view)) {
+                    Log.d("Collision", "Collision détectée !");
+                }
+            }
+        }
+    }
+
+    private boolean checkCollision(ImageView asteroid) {
+        Rect spaceshipRect = new Rect((int) spaceship.getX(), (int) spaceship.getY(),
+                (int) (spaceship.getX() + spaceship.getWidth()),
+                (int) (spaceship.getY() + spaceship.getHeight()));
+        Rect asteroidRect = new Rect((int) asteroid.getX(), (int) asteroid.getY(),
+                (int) (asteroid.getX() + asteroid.getWidth()),
+                (int) (asteroid.getY() + asteroid.getHeight()));
+
+        return spaceshipRect.intersect(asteroidRect);
     }
 }
